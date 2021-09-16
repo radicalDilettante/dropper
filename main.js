f94913c2e9921b8f6c9919e509e2436a739488cf
WAYNE CHOI <wonjun92@gmail.com>
2021-09-16 23:39:28 +1200

    update class and id name with convention

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const searchForm = document.getElementById("searchForm");
-const cameraButton = document.getElementById("cameraButton");
-const urlInput = document.getElementById("urlInput");
 
-const imageLoader = document.getElementById("imageLoader");
-const imageLoaderClose = document.getElementById("imageLoaderClose");
+const urlInput = document.getElementById("urlInput");
+const cameraButton = document.getElementById("cameraButton");
+
+const imageFile = document.getElementById("imageFile");
+
 const imageUrlHeader = document.getElementById("imageUrlHeader");
 const imageFileHeader = document.getElementById("imageFileHeader");
 const imageUrlLoader = document.getElementById("imageUrlLoader");
-const imageFileLoader = document.getElementById("imageFileLoader");
-
 const imageUrlLoaderForm = document.getElementById("imageUrlLoaderForm");
 const imageUrlInput = document.getElementById("imageUrlInput");
+
+const imageFileLoader = document.getElementById("imageFileLoader");
+
+const imageFileClose = document.getElementById("imageFileClose");
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -25,12 +28,12 @@ searchForm.addEventListener("submit", (e) => {
 
 cameraButton.addEventListener("click", (e) => {
   e.preventDefault();
-  imageLoader.classList.remove("hide");
+  imageFile.classList.remove("hide");
 });
 
-imageLoaderClose.addEventListener("click", (e) => {
+imageFileClose.addEventListener("click", (e) => {
   e.preventDefault();
-  imageLoader.classList.add("hide");
+  imageFile.classList.add("hide");
 });
 
 imageUrlHeader.addEventListener("click", (e) => {
