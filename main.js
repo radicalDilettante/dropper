80231ec6f322f24f7eaae319040514600baec507
WAYNE CHOI <wonjun92@gmail.com>
2021-09-16 15:19:57 +1200

    markup and style

--- main.js
+++ main.js
@@ -1,21 +1,64 @@
 const searchForm = document.getElementById("searchForm");
-const searchButton = document.getElementById("searchButton");
 const cameraButton = document.getElementById("cameraButton");
-const input = document.getElementById("url");
-const locationWithUrl = (url) => {
-  return `/search.html?url=${url}`;
-};
+const urlInput = document.getElementById("urlInput");
 
-cameraButton.addEventListener("click", openCameraFeature);
+const imageLoader = document.getElementById("imageLoader");
+const imageLoaderClose = document.getElementById("imageLoaderClose");
+const imageUrlHeader = document.getElementById("imageUrlHeader");
+const imageFileHeader = document.getElementById("imageFileHeader");
+const imageUrlLoader = document.getElementById("imageUrlLoader");
+const imageFileLoader = document.getElementById("imageFileLoader");
+
+const imageUrlLoaderForm = document.getElementById("imageUrlLoaderForm");
+const imageUrlInput = document.getElementById("imageUrlInput");
+
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (input.value === "") {
-    location.href = locationWithUrl(input.placeholder);
+  let url;
+  if (urlInput.value === "") {
+    url = urlInput.placeholder;
   } else {
-    location.href = locationWithUrl(input.value);
+    url = urlInput.value;
   }
+  // Load Image
 });
 
-function openCameraFeature(e) {
+cameraButton.addEventListener("click", (e) => {
   e.preventDefault();
-}
+  imageLoader.classList.remove("hide");
+});
+
+imageLoaderClose.addEventListener("click", (e) => {
+  e.preventDefault();
+  imageLoader.classList.add("hide");
+});
+
+imageUrlHeader.addEventListener("click", (e) => {
+  e.preventDefault();
+  imageUrlHeader.classList.remove("whiteBg");
+  imageUrlHeader.classList.add("greyBg");
+  imageFileHeader.classList.remove("greyBg");
+  imageFileHeader.classList.add("whiteBg");
+  imageUrlLoader.classList.remove("hide");
+  imageFileLoader.classList.add("hide");
+});
+imageFileHeader.addEventListener("click", (e) => {
+  e.preventDefault();
+  imageFileHeader.classList.remove("whiteBg");
+  imageFileHeader.classList.add("greyBg");
+  imageUrlHeader.classList.remove("greyBg");
+  imageUrlHeader.classList.add("whiteBg");
+  imageFileLoader.classList.remove("hide");
+  imageUrlLoader.classList.add("hide");
+});
+
+imageUrlLoaderForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let url;
+  if (imageUrlInput.value === "") {
+    url = imageUrlInput.placeholder;
+  } else {
+    url = imageUrlInput.value;
+  }
+  // Load Image
+});
