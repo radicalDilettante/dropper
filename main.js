1a13e8aaf63190afbc33ed1d65a52679bb197f05
WAYNE CHOI <wonjun92@gmail.com>
2021-09-18 23:13:39 +1200

    change mousemove event logic

--- main.js
+++ main.js
@@ -113,18 +113,6 @@ resultClose.addEventListener("click", (e) => {
   // ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
-function getPosition(obj) {
-  let curLeft = 0,
-    curTop = 0;
-  if (obj.offsetParent) {
-    do {
-      curLeft += obj.offsetLeft;
-      curTop += obj.offsetTop;
-    } while ((obj = obj.offsetParent));
-    return { x: curLeft, y: curTop };
-  }
-  return undefined;
-}
 function rgbToHex(r, g, b) {
   if (r > 255 || g > 255 || b > 255) throw "Invalid color component";
   return ((r << 16) | (g << 8) | b).toString(16);
@@ -132,9 +120,8 @@ function rgbToHex(r, g, b) {
 canvas.addEventListener(
   "mousemove",
   function (e) {
-    let pos = getPosition(this);
-    let x = e.pageX - pos.x;
-    let y = e.pageY - pos.y;
+    let x = e.offsetX;
+    let y = e.offsetY;
     let coord = "x=" + x + ", y=" + y;
     let c = this.getContext("2d");
     let p = c.getImageData(x, y, 1, 1).data;
